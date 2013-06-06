e1444ebb331d03e8c07e2a8754f2309d10bbd127
Ashok Kumar <k.ashok502@gmail.com>
2013-06-06 14:31:11 +0530

    deploy to amazon server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,22 @@
-set :default_stage, "production"
-require 'capistrano/ext/multistage'
-require 'bundler/capistrano'
-require 'rvm/capistrano'
+#require 'capistrano/ext/multistage'
+#require 'bundler/capistrano'
+#require 'rvm/capistrano'
 
-set :application, "enods"
+set :application, "/var/www/enods.com/public_html"
 set :repository,  "git@github.com:neeraji2it/enods.git"
+set :git_enable_submodules, 1 # if you have vendored rails
 set :scm, :git
-set :scm_user, "rails"
+set :scm_user, "ubuntu"
+set :scm_verbose, true
+# roles (servers)
+role :web, '54.214.252.107'
+role :app, '54.214.252.107'
+role :db,  '54.214.252.107', :primary => true
+
+# deploy config
+set :deploy_to, application
+set :deploy_via, :export
+ 
 
 desc "Symlinks database.yml, mailer.yml file from shared directory into the latest release"
 task :symlink_shared, :roles => [:app, :db] do
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :domain, "ec2-54-214-252-107.us-west-2.compute.amazonaws.com"
+set :domain, "ec2-54-244-135-57.us-west-2.compute.amazonaws.com"
 
 set :deploy_to, "/var/www/enods.com/public_html"
 
@@ -6,7 +6,7 @@ set :user, "rails"
 set :use_sudo, false
 
 set :rails_env, "production"
-role :app, "ec2-54-214-252-107.us-west-2.compute.amazonaws.com"
-role :web, "ec2-54-214-252-107.us-west-2.compute.amazonaws.com"
-role :db,  "ec2-54-214-252-107.us-west-2.compute.amazonaws.com", :primary => true
+role :app, "ec2-54-244-135-57.us-west-2.compute.amazonaws.com"
+role :web, "ec2-54-244-135-57.us-west-2.compute.amazonaws.com"
+role :db,  "ec2-54-244-135-57.us-west-2.compute.amazonaws.com", :primary => true
   
