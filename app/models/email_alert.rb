13b3890ed7105a0bd7d6848ad73deacca3e752e1
Ashok Kumar <k.ashok502@gmail.com>
2013-06-10 13:56:44 +0530

    some modifications in the model

--- app/models/email_alert.rb
+++ app/models/email_alert.rb
@@ -1,4 +1,4 @@
 class EmailAlert < ActiveRecord::Base
   attr_accessible :email
-  validates :email, :uniqueness => true, :presence => true
+  validates :email, :uniqueness => true,:format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }, :presence => true
 end
