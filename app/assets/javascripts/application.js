c3b88f8fa0ac957cd2cb81b22daa5e8e655f7d29
Ashok Kumar <k.ashok502@gmail.com>
2013-07-15 18:51:15 +0530

    small changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,7 @@ function add_fields(link, association, content){
     var new_id = new Date().getTime();
     var regexp = new RegExp("new_" + association, "g")
     $(link).parent().before(content.replace(regexp, new_id));
+    $(".uniform").uniform();
 }
 
 //ajax loader
