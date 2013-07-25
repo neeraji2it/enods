99965b234594236e80aee2e265233dc0f593a1dc
rails <rails@my-desktop.(none)>
2013-07-25 17:22:48 +0530

    small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => {:thumb => '90*90>', :large => '900*900>'}, :default_url => "/assets/img-seller-infomation.jpg" if Rails.env == 'development'
   has_attached_file :avatar,:whiny => false,:storage => :s3,:s3_credentials => "#{Rails.root}/config/s3.yml",:path => "uploaded_files/profile/:id/:style/:basename.:extension",:bucket => "enods-web",:styles => {:original => "900x900>",:default => "280x190>",:other => "96x96>"}, :default_url => "/assets/img-seller-infomation.jpg" if Rails.env == 'production'
-  validates :first_name,:last_name,:gender,:date_of_birth,:city,:zip, :presence => {:if => :buyer?}, :on => :update
-  validates :business_name, :business_address, :website_url, :paypal_id, :presence => {:if => :seller?}, :on => :update
+  validates :first_name,:last_name,:gender,:date_of_birth,:phone,:city,:zip, :presence => {:if => :buyer?}, :on => :update
+  validates :business_name, :business_address, :website_url,:phone, :paypal_id, :presence => {:if => :seller?}, :on => :update
 
 
   def buyer?
