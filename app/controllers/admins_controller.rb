897fa37431efc4dfeefa900cc497e5620c3cbf23
Ashok Kumar <k.ashok502@gmail.com>
2013-12-04 16:08:15 +0530

    impliment the start date to end date graph

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,15 +4,9 @@ class AdminsController < ApplicationController
   def admin_dashboard
     @pending_products = Product.where("status = 'pending'").paginate :page => params[:pending_product], :per_page => 4
     @orders = Order.where("status = 'Success' or status = 'Cancel'").paginate :page => params[:latest_order], :per_page => 8
-    @data = (1.month.ago.to_datetime.beginning_of_day..Time.zone.now.end_of_day).map { |date| User.total_on(date).to_f}.inspect
-    @pointInterval = 1.day * 1000
-    @pointStart = 1.month.ago.at_midnight.to_i * 1000
   end
   
   def week
-    @data = (params[:from_date].to_datetime.beginning_of_day..params[:to_date].to_datetime.end_of_day).map { |date| User.total_on(date).to_f}.inspect
-    @pointInterval = 1.day.to_i * 1000
-    @pointStart = params[:from_date].to_datetime.beginning_of_day.to_i * 1000
     respond_to do |format|
       format.js
     end
