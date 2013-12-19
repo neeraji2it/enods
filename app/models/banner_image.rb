bf2c3ee0aa964ed2beb4a0e49a61a6b5488f5b73
Ashok Kumar <k.ashok502@gmail.com>
2013-12-19 14:44:17 +0530

    impliment the reviews and ratings

--- app/models/banner_image.rb
+++ app/models/banner_image.rb
@@ -1,6 +1,6 @@
 class BannerImage < ActiveRecord::Base
   attr_accessible :image,:image_description
-  has_attached_file :image, :style => {:thumb => "90*90>", :large => "900*900>"} if Rails.env == 'development'
+  has_attached_file :image, :styles => {:thumb => "90*90>", :large => "900*900>"} if Rails.env == 'development'
   has_attached_file :image,:whiny => false,:storage => :s3,:s3_credentials => "#{Rails.root}/config/s3.yml",:path => "uploaded_files/profile/:id/:style/:basename.:extension",:bucket => "enods-web",:styles => {:original => "900x900>",:default => "280x190>",:other => "96x96>"} if Rails.env == 'production'
   validates_attachment_content_type :image,
     :content_type => [ 'image/jpeg','image/jpg', 'image/png', 'image/gif','image/bmp', 'image/x-png', 'image/pjpeg' ]
