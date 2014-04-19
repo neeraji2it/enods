b3eba36335cf11fe54a486bb1e361a86564f1a78
neeraji2it <neeraji2it@gmail.com>
2014-04-19 12:27:17 +0530

    small changes

--- app/controllers/constants_controller.rb
+++ app/controllers/constants_controller.rb
@@ -5,7 +5,7 @@ class ConstantsController < ApplicationController
     @products = Product.where("user_id = #{@user.id} and status = 'confirmed'").order("created_at Desc")
     @current_user = current_user
     @follow = Follow.find_by_user_id_and_receiver_id(@current_user.id,@user.id)
-    @follow.present? ? (@follow.update_attribute(:status, @follow.status == 'Follow' ? 'Unfollow' : 'Follow')) : (Follow.create(:user_id => @current_user.id, :receiver_id => @user.id, :status => 'Follow'))
+    @follow.present? ? (@follow.update_attribute(:status, @follow.status == 'Follow' ? 'Unfollow' : 'Follow')) : (Follow.create(:user_id => @current_user.id, :receiver_id => @user.id, :status => 'Follow')) if @user.id != @current_user.id
     redirect_to seller_path(@user)
   end
   
@@ -14,7 +14,7 @@ class ConstantsController < ApplicationController
     @products = Product.where("user_id = #{@user.id} and status = 'confirmed'").order("created_at Desc")
     @current_user = current_user
     @follow = FavoriteUser.find_by_user_id_and_receiver_id(@current_user.id,@user.id)
-    @follow.present? ? (@follow.update_attribute(:status, @follow.status == 'Favorited' ? 'Favorite' : 'Favorited')) : (FavoriteUser.create(:user_id => @current_user.id, :receiver_id => @user.id, :status => 'Favorited'))
+    @follow.present? ? (@follow.update_attribute(:status, @follow.status == 'Favorited' ? 'Favorite' : 'Favorited')) : (FavoriteUser.create(:user_id => @current_user.id, :receiver_id => @user.id, :status => 'Favorited'))  if @user.id != @current_user.id
     redirect_to seller_path(@user)
   end
 end
